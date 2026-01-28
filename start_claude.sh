5ca205a36c572511afd920e67a26a4c58c9f9452
charketh <hvbjpyrbk3580@hotmail.com>
2026-01-28 22:20:34 +0800

    Update start_claude.sh-api

--- start_claude.sh
+++ start_claude.sh
@@ -3,8 +3,8 @@ set -e
 #!/bin/bash
 
 # 设置环境变量
-export ANTHROPIC_AUTH_TOKEN=sk-W2dlfofLnJfkoe26haOiYQmzqFD0DQPBPmfZao8YOBuo0d92
-export ANTHROPIC_BASE_URL=https://apiHK.Ai
+export ANTHROPIC_AUTH_TOKEN=sk-ja5JVy4Ou3lk4ascc0wikoEDthXRJcq5Qk5quewaFaxVMR1a
+export ANTHROPIC_BASE_URL=https://newapi.585dg.com
 # 备选API线路（去掉下面两行注释生效）
 # export ANTHROPIC_AUTH_TOKEN=sk-BnACKGNS5fruSd9Mmm2cH4MuLXGR27sB0md9KsYN6fvR4CYi
 # export ANTHROPIC_BASE_URL=https://xinghuapi.com
